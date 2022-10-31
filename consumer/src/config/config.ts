bdac0854b5a3002c40e419a252486fc99b5c1bfb
F2256342 Daniel de Oliveira Carvalho <daniel_carvalho@bb.com.br>
2022-10-31 12:38:43 -0300

    working state

--- consumer/src/config/config.ts
+++ consumer/src/config/config.ts
@@ -1,29 +1,7 @@
-import { IEnvironment } from "environment.js";
+import { IConfig } from "types/config.js";
 
 const CONFIG = {
-    SCRAPE_TARGET_URL: "https://mobi2.bb.com.br/cfe-acesso/api/v1/info/status",
-    DB: {
-        MONGODB_HOST: "localhost",
-        MONGODB_PORT: 27017,
-        MONGODB_USER: "root",
-        MONGODB_PASS: "pleasehashapasswordomg",
-        MONGODB_DB: "store"
-    },
-    CRONTAB: {
-        DAILY_12AM: {
-            EXP: "0 12 * * *",
-            DESCRIPTION: "every day at 12:00"
-        },
-        EVERY_HOUR: {
-            EXP: "0 * * * *",
-            DESCRIPTION: "every hour at 0 minutes"
-        },
-        EVERY_MINUTE: {
-            EXP: "* * * * *",
-            DESCRIPTION: "every minute"
-        },
-
-    }
+    TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN
 };
 
-export default CONFIG as IEnvironment;
+export default CONFIG as IConfig;
---
+++ consumer/src/types/config.d.ts
@@ -0,0 +1,5 @@
+// config.d.ts file
+
+export interface IConfig {
+    TELEGRAM_TOKEN: string;
+}

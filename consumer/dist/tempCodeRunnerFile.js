9bf8c5661fdb3afe585d5b7d0919dda9589893ff
Daniel de O. Carvalho <yutsuo@gmail.com>
2022-11-01 22:42:02 -0300

    cleanup

--- consumer/dist/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-export const coordGenerator2 = () => {
-    return parseFloat((Math.random() * (180 - (-180)) + (-180)).toFixed(6));
-};
-console.log(coordGenerator2());
-//# sourceMappingURL=tempCodeRunnerFile.js.map
--- consumer/dist/tests.js
+++
@@ -1,9 +0,0 @@
-export const coordGenerator = () => {
-    return parseFloat((Math.random() * (180 - (-180)) + (-180)).toFixed(6));
-};
-let gMapsURL = `https://www.google.com/maps/search/?api=1&query=${(coordGenerator())}%2C${coordGenerator()}`;
-console.log(coordGenerator());
-setInterval(() => {
-    console.log(`https://www.google.com/maps/search/?api=1&query=${(coordGenerator())}%2C${coordGenerator()}`);
-}, 1000);
-//# sourceMappingURL=tests.js.map
